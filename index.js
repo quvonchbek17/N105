74d6ce6aabd9a7356335c8531531da32dbe1ccf7
AlisherOlimjonov <alwaysusefrompc@gmail.com>
2023-06-05 10:26:31 +0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//// uyga vazifa
+function uniceItem(arr) {
+  let newArr = [];
+  arr.map((item) => {
+    if (!newArr.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr.sort((a, b) => a - b);
+}
+
+console.log(uniceItem([1, , 3, 3, 4, 2, 3, 4, 4]));
