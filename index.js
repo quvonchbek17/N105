6e49ec56e8589e8ba52d612946b538f709b73d43
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2023-06-05 10:20:34 +0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http")
 const server = http.createServer((req, res) => {
    let method = req.method
+   let url = req.url
 })
 
 server.listen(9000, () => console.log("Ishladi"))
