4a1d67a6c23bfa38492d4edee1fd576f7811888e
Jaloliddin-Gulomov <jaloliddingulomov7061@gmail.com>
2023-06-07 00:48:23 +0500

    Jaloliddin's homework

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = [
+
+    {
+        "id": "1",
+        "type": "fantastik",
+        "title": "Harry Potter"
+    },
+    {
+        "id": "2",
+        "type": "fantastik",
+        "title": "Jumanji"
+    },
+    {
+        "id": "3",
+        "type": "detektiv",
+        "title": "Sherlock Holmes"
+    },
+    {
+        "id": "4",
+        "type": "detektiv",
+        "title": "Ibtido"
+    },
+    {
+        "id": "5",
+        "type": "komik",
+        "title": "Hobbitlar"
+    }
+
+]
--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-//// uyga vazifa
-//nimadir
+const { createServer } = require('http')
+const http = require('http')
+const data = require('./data.js')
+const { stringify } = require('querystring')
+createServer((req, response) => {
+    const method = req.method
+    const url = req.url
+    const bookType = url.split('/')[2]
+    const bookId = url.split('/')[3]
+    if(method == 'GET'){
+        if(url.split("/")[1] === "books"){
+            if(!bookType){
+                response.end(JSON.stringify(data))
+                return
+            }
+            if(!bookId){
+                response.end(JSON.stringify(data))
+                return
+            }
+
+            const book = data.find(e => e.id == bookId)
+            
+            if(!book){
+                response.writeHead(404, {
+                    "Content-type": "application/json"
+                })
+                response.end(JSON.stringify({
+                    status: 404,
+                    message: http.STATUS_CODES[404]
+                }))
+            }
+
+            response.writeHead(200, {
+                'Content-type': 'application/json'
+            })
+
+            response.end(JSON.stringify(book))
+            return
+        }
+
+        response.end("Get method")
+    }else{
+        response.end("modification method")
+    }
+})
+.listen(9000, () => {
+    console.log("listening...");
+})
