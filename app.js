b793fd56ac8daa428067cfed5e830119564a5aef
zafarkh <hatamovzafar7070@gmail.com>
2023-06-07 00:29:54 +0500

    hometask-done

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log('NodeJs has started');
+const http = require("http");
+const data = require("./data");
+
+http
+  .createServer((request, response) => {
+    const method = request.method;
+    const url = request.url;
+    const bookType = url.split("/")[2];
+    const bookId = url.split("/")[3];
+
+    if (method === "GET") {
+      if (url.split("/")[1] === "books") {
+        if (!bookType) {
+          response.end(JSON.stringify(data));
+          return;
+        }
+        if (!bookId) {
+          const booksOfType = data[0][bookType];
+          if (!booksOfType) {
+            response.writeHead(404, {
+              "Content-type": "application/json",
+            });
+            response.end(
+              JSON.stringify({
+                status: 404,
+                message: "Book type not found",
+              })
+            );
+          }
+          response.end(JSON.stringify(booksOfType));
+          return;
+        }
+        const book = data[0][bookType].find((e) => e.id == bookId);
+        if (!book) {
+          response.writeHead(404, {
+            "Content-type": "application/json",
+          });
+          response.end(
+            JSON.stringify({
+              status: 404,
+              message: "Book not found",
+            })
+          );
+        }
+        response.writeHead(200, {
+          "Content-type": "application/json",
+        });
+        response.end(JSON.stringify(book));
+        return;
+      }
+      response.end("GET method");
+    } else {
+      response.end("Modification method");
+    }
+  })
+  .listen(9000, () => {
+    console.log("Listening...");
+  });
---
+++ data.js
@@ -0,0 +1,48 @@
+module.exports = [
+  {
+    bestseller: [
+      {
+        id: 1,
+        title: "IDENTITY",
+      },
+      {
+        id: 2,
+        title: "HAPPY PLACE",
+      },
+      {
+        id: 3,
+        title: "IT ENDS WITH US",
+      },
+      {
+        id: 4,
+        title: "THE COVENANT OF WATER",
+      },
+      {
+        id: 5,
+        title: "THE WAGER",
+      },
+    ],
+    fiction: [
+      {
+        id: 1,
+        title: "VERITY",
+      },
+      {
+        id: 2,
+        title: "THE SEVEN HUSBANDS OF EVELYN HUGO",
+      },
+      {
+        id: 3,
+        title: "MEET ME AT THE LAKE",
+      },
+      {
+        id: 4,
+        title: "MAGNOLIA TABLE, VOL. 3",
+      },
+      {
+        id: 5,
+        title: "THE CREATIVE ACT",
+      },
+    ],
+  },
+];
